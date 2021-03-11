267e40fb45d3bd7f91464e2f27972414484ca25e
richietownsend <richie.townsend@gmail.com>
2021-03-11 09:41:01 -0500

    updated for mac

--- data/richie_queries.sql
+++ data/richie_queries.sql
@@ -108,14 +108,97 @@ ORDER BY stolen_bases_perc DESC;
 --A6 Chris Owings with 91% stolen_bases_perc
 
 --Q7
+/*
 SELECT yearid, franchid, g, w, l, wswin
 FROM teams
-WHERE yearid BETWEEN 1970 and 2016 AND yearid <> 1994 AND yearid <> 1981
-ORDER BY w DESC;
+WHERE yearid BETWEEN 1970 and 2016 AND yearid <> 1994
+ORDER BY yearid, w DESC;
+*/
 -- A7(A) 116 wins by Seattle in 2001 (did not win World Series)
 -- A7(B) 63 wins by LA Dodgers in 1981 (Won the World Series) - 60 less games that season
 		-- STRIKE from June 12 - July 31 1981
-		-- Stirike in August of 1994. NO post season.
---A7(C)
+		-- STRIKE in August of 1994. NO post season.
+--A7(C) 45/46 RECORDS 11/11 OR .24444/.23913. Less than a quarter of the time
+--1970, Y
+--1971, N
+--1972, N
+--1973, N
+--1974, N
+--1975, Y
+--1976, Y
+--1977, N
+--1978, Y
+--1979, N
+--1980, N
+--1982, N
+--1983, N
+--1984, Y
+--1985, N
+--1986, Y
+--1987, N
+--1988, N
+--1989, Y
+--1990, N
+--1991, N
+--1992, N
+--1993, N
+--1995, N
+--1996, N
+--1997, N
+--1998, Y
+--1999, N
+--2000, N
+--2001, N
+--2002, N
+--2003, N
+--2004, N
+--2005, N
+--2006, N
+--2007, Y
+--2008, N
+--2009, Y
+--2010, N
+--2011, N
+--2012, N
+--2013, N
+--2014, N
+--2015, N
+--2016, Y
 
+--Q8
+/*
+SELECT h.year, tf.franchname, p.park_name, games, h.attendance, ROUND((h.attendance*1.0)/(games*1.0),2) AS avg_attendance_game
+FROM homegames AS h
+JOIN parks AS p
+ON h.park = p.park
+LEFT JOIN teams AS t
+ON h.team = t.teamid
+LEFT JOIN teamsfranchises AS tf
+ON t.franchid = tf.franchid
+WHERE year = 2016 AND games > 10
+GROUP BY h.team, tf.franchname, p.park_name, games, h.attendance, avg_attendance_game, h.year
+ORDER BY avg_attendance_game
+LIMIT 5;
+*/
+--A8 Dodger Stadium, LA Dodgers AVG of 45,719.9 per game.
+--Lowest attendance Tampa Bay Rays; Tropicana Field 15,878.56/game
+--Oakland As; Oakland-Alameda County Coliseum 18,784.02/game
+--Cleveland Indians; Progressive Field 19,650.21/game
+--Florida Marlins; Marlins Park 21,405.21/game
+--Chicago White Sox; U.S. Cellular Field 21,559.17
+
+--Q9
+SELECT sub.namefirst, sub.namefirst, sub.namelast, sub.n_awards, am.lgid
+FROM
+(SELECT am.playerid, p.namefirst, p.namelast, am.lgid, COUNT(awardid), COUNT(awardid) OVER(PARTITION BY am.playerid ORDER BY am.playerid) AS n_awards
+FROM awardsmanagers AS am
+JOIN people AS p
+ON am.playerid = p.playerid
+GROUP BY am.playerid, p.namefirst, p.namelast, am.lgid, awardid
+ORDER BY playerid) AS sub
+JOIN awardsmanagers AS am
+ON am.playerid = sub.playerid
+GROUP BY sub.n_awards, sub.namefirst, sub.namefirst, sub.namelast, am.lgid
+ORDER BY sub.n_awards DESC;
+--A9
 
